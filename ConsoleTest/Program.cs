0a3f336f2a63a5de9a58799ac97dac0482d62127
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-01-30 20:18:45 +0300

    Тестовая программа WPF для отправки почты

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -4,13 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.Net.Mail;
+
 namespace ConsoleTest
 {
     class Program
     {
         static void Main(string[] args)
         {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("shmachilin@yandex.ru", "Павел");
+            message.To.Add("shmachilin@gmail.com");
 
+            message.Subject = "Заголовок сообщения";
+            message.Body = "Текст сообщения";
+            message.IsBodyHtml = false;
+
+            SmtpClient client = new SmtpClient("smtp.yandex.ru", 465);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("username@yandex.ru", "password");
+
+            client.Send(message); 
 
             Console.ReadLine();
         }
