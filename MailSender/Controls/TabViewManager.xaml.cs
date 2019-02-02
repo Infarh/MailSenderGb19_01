3884e39212690c14bd69a93aa743328b3e0270fd
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-02-02 19:36:27 +0300

    Пользовательский элемент управления - менеджер вкладок

---
+++ MailSender/Controls/TabViewManager.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows;
+
+namespace MailSender.Controls
+{
+    public partial class TabViewManager
+    {
+        public event EventHandler Forvard;
+
+        public event EventHandler Backvard;
+
+        public TabViewManager() => InitializeComponent();
+
+        private void BackwardButtonClick(object Sender, RoutedEventArgs E)
+        {
+            //Forvard?.Invoke(this, EventArgs.Empty);
+            var handlers = Forvard;
+            if(handlers != null)
+                handlers(this, EventArgs.Empty);
+        }
+
+        private void ForvardButtonClick(object Sender, RoutedEventArgs E)
+        {
+            Backvard?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
--- MailSender/MainWindow.xaml.cs
+++ MailSender/MainWindow.xaml.cs
@@ -1,4 +1,21 @@
-﻿namespace MailSender
+﻿using System;
+
+namespace MailSender
 {
-    public partial class MainWindow  { public MainWindow() => InitializeComponent(); }
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void OnForvard(object Sender, EventArgs E)
+        {
+            if (MainTabControl.SelectedIndex > 0)
+                MainTabControl.SelectedIndex--;
+        }
+
+        private void OnBackvard(object Sender, EventArgs E)
+        {
+            if (MainTabControl.SelectedIndex < MainTabControl.Items.Count)
+                MainTabControl.SelectedIndex++;
+        }
+    }
 }
