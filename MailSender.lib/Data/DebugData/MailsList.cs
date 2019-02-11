ee6d3d19fc1bbc689b18b8cd2ec0431b139e526b
jonca3d <jonca3d@gmail.com>
2019-02-12 00:53:09 +0300

    Задание с самодельным контролом в ListView

---
+++ MailSender.lib/Data/DebugData/MailsList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.VisualStyles;
+
+namespace MailSender.lib.Data.DebugData
+{
+   
+
+    public static class MailsList
+    {
+        public static ObservableCollection<Mail> Items { get; set; } = new ObservableCollection<Mail>
+        {
+            new Mail{strMessage = "1", dtDate = DateTime.Now},
+            new Mail{strMessage = "2", dtDate = DateTime.Now},
+            new Mail{strMessage = "3", dtDate = DateTime.Now},
+            new Mail{strMessage = "4", dtDate = DateTime.Now},
+
+        };
+    }
+
+    public class Mail
+    {
+        public string strMessage { get; set; }
+        public DateTime dtDate { get; set; }
+    }
+}
---
+++ MailSender/Controls/ListViewItemScheduler.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MailSender.Controls
+{
+    /// <summary>
+    /// Interaction logic for ListViewItemScheduler.xaml
+    /// </summary>
+    public partial class ListViewItemScheduler : UserControl
+    {
+        public ListViewItemScheduler() => InitializeComponent();
+
+        public event EventHandler AddMessage;
+
+        public event EventHandler RemoveElement;
+
+        private void AddMessageClick(object sender, RoutedEventArgs E)
+        {
+            AddMessage?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void RemoveElementClick(object sender, RoutedEventArgs E)
+        {
+            RemoveElement?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
