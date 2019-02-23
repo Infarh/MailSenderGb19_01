1cd51837a502bf260429fa86f2b2cb71c80cfb2e
Шмачилин Павел Александрович <shmachilin@gmail.com>
2019-02-23 17:00:10 +0300

    Подготовка к 8 уроку

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -6,14 +6,6 @@ namespace ConsoleTest
     {
         private static void Main(string[] args)
         {
-            var report = new Report
-            {
-                StrData = "My string data!",
-                IntData = 42
-            };
-
-            report.CreatePackage("report.docx");
-
             Console.ReadLine();
         }
     }
